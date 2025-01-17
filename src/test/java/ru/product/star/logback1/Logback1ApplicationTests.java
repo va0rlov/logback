14eb7cd1fd6c0996f4f66bffae0242899f4111d4
Виктор Орлов <vorlov@topsbi.ru>
2025-01-17 09:36:01 +0300

    master initial commit

---
+++ src/test/java/ru/product/star/logback1/Logback1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.product.star.logback1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Logback1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
